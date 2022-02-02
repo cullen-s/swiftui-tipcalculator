a5e3832b711d879bf53ac2f55571b2ad5187ee2f
cullensteber <steber@uab.edu>
2022-02-02 08:30:32 -0600

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Cullen Steber on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
